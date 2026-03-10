e2b14ff9b10b9973aa473458b1264222c1b1c95d
Vinay S <73406313+vinays6@users.noreply.github.com>
2026-03-09 20:59:12 -0700

    Fix navbar styling

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -8,14 +8,14 @@
 <nav class="flex w-full bg-base-200">
   <a href="/" class="px-4 py-3 text-2xl font-bold">❤️❤️❤️</a>
   <div class="flex flex-grow"></div>
-  <a href="/search" class="px-4 py-2 text-2xl font-bold">Search</a>
-  <a href="/profile" class="px-4 py-2 text-2xl font-bold">Profile</a>
+  <a href="/search" class="inline-flex items-center px-4 py-2 text-2xl font-bold">Search</a>
+  <a href="/profile" class="inline-flex items-center px-4 py-2 text-2xl font-bold">Profile</a>
   {#if userState.loggedIn}
     <button 
-       class="px-4 py-2 text-2xl font-bold cursor-pointer" 
+       class="inline-flex items-center px-4 py-2 text-2xl font-bold cursor-pointer inline-block" 
        onclick={() => (logoutOpen = !logoutOpen)}
        >
-       Logged in as <span class="text-primary">{userState.name}</span>
+       Logged in as<span class="text-primary">&nbsp;{userState.name}</span>
     </button>
     {#if logoutOpen}
       <div class="absolute right-4 top-14 w-48 bg-base-200 rounded-md shadow-lg">
@@ -35,6 +35,6 @@
       </div>
     {/if}
   {:else}
-    <a href="/login" class="px-4 py-2 text-2xl font-bold">Login</a>
+    <a href="/login" class="inline-flex items-center px-4 py-2 text-2xl font-bold">Login</a>
   {/if}
 </nav>
